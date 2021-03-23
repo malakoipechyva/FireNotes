7e4819d72a0f9e691a4d0ac1787f678f1ca3b780
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-03-23 10:12:42 +0300

    initial commit

---
+++ FireNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireNotes
+//
+//  Created by malakoipechyva on 23.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
